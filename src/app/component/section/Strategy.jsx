8200654cc5b3578f1c1323fc9e8695d0fb96477b
haznymhd <145745688+haznymhd@users.noreply.github.com>
2024-10-02 23:56:55 +0530

    update

--- src/app/component/section/Strategy.jsx
+++ src/app/component/section/Strategy.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Strategy = () => {
   return (
-    <section className="containers flex flex-col md:flex-row-reverse gap-4 ">
+    <section className="containers flex flex-col md:flex-row-reverse gap-4 md:items-center md:justify-center">
       <Image
-        className=" object-cover object-center md:w-[245px]"
+        className="object-cover object-center md:w-[245px] mx-auto md:mx-0"
         src="/be108bd31437b72eaae8e1dbd7abd910.png"
         alt="Web & Mobile App Development"
         width={414}
@@ -13,13 +13,13 @@ const Strategy = () => {
         priority
       />
 
-      <div className="flex items-center justify-between mr-auto">
-        <div className="flex flex-col gap-3 max-w-[500px]">
+      <div className="flex items-center justify-between mr-auto md:text-center md:items-center md:justify-center ml-auto md:ml-0 mx-auto md:mx-0">
+        <div className="flex flex-col gap-3 max-w-[500px] text-center md:text-left">
           <h2 className="font-semibold text-bluishPurple text-2xl">
             Digital Strategy Consulting
           </h2>
 
-          <p className=" font-normal text-sm text-wrap text-tricornBlack-900">
+          <p className="font-normal text-sm text-wrap text-tricornBlack-900">
             Your digital strategy should complement the overall marketing
             strategy of the company. In online marketing, each component will
             never work in isolation and every business needs a different mix. We
@@ -27,12 +27,14 @@ const Strategy = () => {
             efficient model for your business.
           </p>
 
-          <Link
-            href="/"
-            className=" bg-yuzuJam hover:bg-yuzuJam text-sm text-white font-bold uppercase py-2 px-4 rounded-md w-fit"
-          >
-            Learn More
-          </Link>
+          <div className="ml:flex ml:justify-center">
+            <Link
+              href="/"
+              className="bg-yuzuJam hover:bg-yuzuJam text-sm text-white font-bold uppercase py-2 px-4 rounded-md w-fit"
+            >
+              Learn More
+            </Link>
+          </div>
         </div>
       </div>
     </section>
--- src/app/component/section/Web.jsx
+++ src/app/component/section/Web.jsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 
 const Web = () => {
   return (
-    <section className="containers flex flex-col md:flex-row gap-4 ">
-      <Image
+    <section className="containers flex flex-col md:flex-row gap-4 md:items-center md:justify-center ">
+      <Image 
+        className="mx-auto md:mx-0"
         src="/c2149f00214af5481c570aeb1a76d7d3.png"
         alt="Web & Mobile App Development"
         width={414}
@@ -12,25 +13,26 @@ const Web = () => {
         priority
       />
 
-      <div className="flex items-center justify-between ml-auto">
-        <div className="flex flex-col gap-3 max-w-[500px]">
-          <h2 className=" font-semibold text-bluishPurple text-2xl">
+      <div className="flex items-center justify-between ml-auto md:ml-0 mx-auto md:mx-0">
+        <div className="flex flex-col gap-3 max-w-[500px] text-center md:text-left">
+          <h2 className="font-semibold text-bluishPurple text-2xl">
             Web & Mobile App Development
           </h2>
 
-          <p className=" font-normal text-sm text-wrap text-tricornBlack-900">
+          <p className="font-normal text-sm text-wrap text-tricornBlack-900">
             Your web and mobile apps are pieces of the puzzle to grow your
             business. We use frameworks that tailor content and engagement
             methods to respond to different interests shown by your potential
             customers who interact with your business online.
           </p>
-
-          <Link
-            href="/"
-            className="bg-yuzuJam hover:bg-yuzuJam text-sm text-white font-bold uppercase py-2 px-4 rounded-md w-fit"
-          >
-            Learn More
-          </Link>
+          <div className="ml:flex ml:justify-center">
+            <Link
+              href="/"
+              className="bg-yuzuJam hover:bg-yuzuJam text-sm text-white font-bold uppercase py-2 px-4 rounded-md w-fit"
+            >
+              Learn More
+            </Link>
+          </div>
         </div>
       </div>
     </section>
