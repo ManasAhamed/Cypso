e015ab24ebbac403f2548ee49a871740f52e20f1
haznymhd <145745688+haznymhd@users.noreply.github.com>
2024-10-02 15:24:16 +0530

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,6 @@
 import Layout from './component/layout';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
 import FAQSection from './FAQSection';
 import DigitalStrategy from './DigitalStrategy';
 import DevelopmentSection from './DevelopmentSection'
