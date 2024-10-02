713e25333ffc00088613ac28a6ce6655d89f079d
haznymhd <145745688+haznymhd@users.noreply.github.com>
2024-10-02 22:41:25 +0530

    update

--- src/app/component/navigation/Header.jsx
+++ src/app/component/navigation/Header.jsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,49 +10,59 @@ const Header = () => {
   };
 
   return (
-    <nav
-    className="bg-bluishPurple text-white flex justify-between items-center relative z-50 
-    2xl:py-6 2xl:px-20 2xl:max-w-[100%] 2xl:h-[77px]
-    xl:py-6 xl:px-16 xl:max-w-[100%] xl:h-[77px]
-    lg:py-6 lg:px-10 lg:max-w-[100%] lg:h-[77px]
-    md:py-6 md:px-8 md:max-w-[100%] md:h-[77px]  
-    xs:py-6 xs:px-5 xs:max-w-[100%] xs:h-[77px]">
-  
-      <div className="flex items-center">
+    <nav className="bg-bluishPurple text-white py-5">
+      <div className="containers flex flex-row justify-between items-center">
         <Link href="/" passHref>
-          <div className="flex items-center">
-            <Image
-              src="/at_digitals_logo.png"
-              alt="AT Digital Logo"
-              width={238.89}
-              height={25}
-              className="object-cover"
-            />
-          </div>
-        </Link>
-      </div>
-
-      <div className="sm:block lg:hidden">
-        <button onClick={toggleMenu} aria-label="Toggle menu">
           <Image
-            src={isMenuOpen ? '/close-icon.png' : '/hamburger-icon.png'}
-            alt={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+            className="object-cover object-center"
+            src="/at_digitals_logo.png"
+            alt="AT Digital Logo"
+            width={240}
+            height={25}
+            priority
+          />
+        </Link>
+
+        <button
+          className="sm:block lg:hidden"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <Image
+            src={isMenuOpen ? "/close-icon.png" : "/hamburger-icon.png"}
+            alt={isMenuOpen ? "Close Menu" : "Open Menu"}
             width={24}
             height={24}
+            priority
           />
         </button>
-      </div>
 
-      <div className="hidden lg:flex space-x-7 text-base uppercase 
-        2xl:gap-[28px] xl:gap-[20px] lg:gap-[15px]"
-        style={{
-          height: '17px',
-          opacity: 1,
-        }}>
-        <Link href="/services">Services</Link>
-        <Link href="/about">About Us</Link>
-        <Link href="/contact">Contact Us</Link>
-        <Link href="/careers">Careers</Link>
+        <div className="hidden lg:flex flex-row gap-10">
+          <Link
+            href="#services"
+            className="font-medium text-base uppercase text-white hover:text-purple-200"
+          >
+            Services
+          </Link>
+          <Link
+            href="#about"
+            className="font-medium text-base uppercase text-white hover:text-purple-200"
+          >
+            About Us
+          </Link>
+          <Link
+            href="#contact"
+            className="font-medium text-base uppercase text-white hover:text-purple-200"
+          >
+            Contact Us
+          </Link>
+          <Link
+            href="#careers"
+            className="font-medium text-base uppercase text-white hover:text-purple-200"
+          >
+            Careers
+          </Link>
+        </div>
       </div>
 
       {isMenuOpen && (
@@ -64,12 +74,42 @@ const Header = () => {
           >
             &#x2715;
           </button>
-          <nav className="flex flex-col space-y-4 text-lg uppercase mt-12 sm:mt-0">
-            <Link href="/" onClick={toggleMenu}>Home</Link>
-            <Link href="/services" onClick={toggleMenu}>Services</Link>
-            <Link href="/about" onClick={toggleMenu}>About Us</Link>
-            <Link href="/contact" onClick={toggleMenu}>Contact Us</Link>
-            <Link href="/careers" onClick={toggleMenu}>Careers</Link>
+          <nav className="flex flex-col space-y-4 uppercase mt-12 sm:mt-0">
+            <Link
+              href="/"
+              className="font-medium text-base uppercase text-gray-950 hover:text-purple-800"
+              onClick={toggleMenu}
+            >
+              Home
+            </Link>
+            <Link
+              href="#services"
+              className="font-medium text-base uppercase text-gray-950 hover:text-purple-800"
+              onClick={toggleMenu}
+            >
+              Services
+            </Link>
+            <Link
+              href="#about"
+              className="font-medium text-base uppercase text-gray-950 hover:text-purple-800"
+              onClick={toggleMenu}
+            >
+              About Us
+            </Link>
+            <Link
+              href="#contact"
+              className="font-medium text-base uppercase text-gray-950 hover:text-purple-800"
+              onClick={toggleMenu}
+            >
+              Contact Us
+            </Link>
+            <Link
+              href="#careers"
+              className="font-medium text-base uppercase text-gray-950 hover:text-purple-800"
+              onClick={toggleMenu}
+            >
+              Careers
+            </Link>
           </nav>
         </div>
       )}
