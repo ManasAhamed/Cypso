2911e58073c35c5e301473eec53c473942dfe58e
haznymhd <145745688+haznymhd@users.noreply.github.com>
2024-10-02 22:40:58 +0530

    update

--- src/app/component/navigation/Footer.jsx
+++ src/app/component/navigation/Footer.jsx
@@ -1,19 +1,21 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="bg-bluishPurple text-white flex flex-col items-center py-10 px-8 w-full sm:w-[100%]">
+    <footer className="bg-bluishPurple text-white flex flex-col items-center py-10 px-8 w-full sm:w-[100%] mt-20">
       <div className="flex flex-col lg:flex-row justify-between w-full max-w-[1280px] gap-8">
         <div className="flex flex-col items-start gap-5 lg:w-1/3">
-          <Image 
-            src="/at_digitals_logo.png" 
-            alt="AT Digital Logo" 
-            width={239} 
-            height={25} 
+          <Image
+            src="/at_digitals_logo.png"
+            alt="AT Digital Logo"
+            width={239}
+            height={25}
           />
           <p className="text-sm max-w-sm leading-snug">
-            Your goal is our target. Not anything in between. We use online marketing platforms and tools to achieve a single objective - your business results.
+            Your goal is our target. Not anything in between. We use online
+            marketing platforms and tools to achieve a single objective - your
+            business results.
           </p>
         </div>
         <div className="flex flex-col items-start lg:w-1/4">
@@ -37,9 +39,13 @@ const Footer = () => {
       <div className="mt-8 w-full max-w-[1280px] sm:w-[100%]">
         <hr className="border-t border-white" />
         <div className="flex justify-center items-center space-x-4 mt-4">
-          <Link href="/privacy-policy" className="text-sm">Privacy Policy</Link>
+          <Link href="/privacy-policy" className="text-sm">
+            Privacy Policy
+          </Link>
           <span className="border-l border-white h-4"></span>
-          <Link href="/terms-conditions" className="text-sm">Terms & Conditions</Link>
+          <Link href="/terms-conditions" className="text-sm">
+            Terms & Conditions
+          </Link>
         </div>
       </div>
     </footer>
